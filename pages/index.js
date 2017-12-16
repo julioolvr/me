c5f07bafb13505015a28e9360afc67952f79c334
Julio Olivera <julio.olvr@gmail.com>
2017-12-16 18:36:40 -0500

    Home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,78 @@
 const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
+  <div className="root">
+    <div className="content">
+      <p className="name">Julio Olivera</p>
+
+      <p className="links">
+        <a>me</a>
+        <span className="separator">|</span>
+        <a>blog</a>
+        <span className="separator">|</span>
+        <a>things</a>
+        <span className="separator">|</span>
+        <a>talks</a>
+        <span className="separator">|</span>
+        <a href="https://github.com/julioolvr">github</a>
+        <span className="separator">|</span>
+        <a href="https://twitter.com/julioolvr">twitter</a>
+      </p>
+    </div>
+
+    <style jsx>{`
+      .root {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+
+      .content {
+        text-align: center;
+      }
+
+      .name {
+        text-transform: uppercase;
+        margin-bottom: 0;
+      }
+
+      .links {
+        margin-top: 0;
+      }
+
+      .separator {
+        margin: 0 0.2em;
+      }
+    `}</style>
+
+    <style jsx global>{`
+      @import url('https://fonts.googleapis.com/css?family=Roboto+Mono:300');
+
+      * {
+        box-sizing: border-box;
+      }
+
+      html,
+      body {
+        margin: 0;
+        padding: 0;
+      }
+
+      body {
+        background-color: #0e0e0e;
+        color: #e3e3e3;
+        font-family: 'Roboto Mono', monospace;
+      }
+
+      a {
+        color: #af3eff;
+        text-decoration: none;
+      }
+
+      a:hover {
+        text-decoration: underline;
+      }
+    `}</style>
   </div>
 );
 
