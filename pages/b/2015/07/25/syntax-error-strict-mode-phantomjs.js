1da2a000e2f0dde095c66144bd7503b448a55482
Julio Olivera <julio.olvr@gmail.com>
2017-12-18 13:33:46 -0500

    Safari bug post

---
+++ pages/b/2015/07/25/syntax-error-strict-mode-phantomjs.js
@@ -0,0 +1,57 @@
+import PostLayout from '~/components/postLayout';
+
+const Post = () => {
+  return (
+    <PostLayout
+      title="Unexplained syntax error on PhantomJS with strict mode"
+      date="2015-07-25"
+      langs={['en']}
+    >
+      {() => {
+        return (
+          <div>
+            <p>
+              Short story: PhantomJS considers a syntax error when a named
+              function expression is called the same as some of its parameters,
+              and won't be very explicit about it.
+            </p>
+
+            <p>Consider the following code:</p>
+
+            <pre>{`
+'use strict';
+var something = function bug(bug) {};
+`}</pre>
+
+            <p>
+              The fact that the function is called <code>bug</code>, and the
+              parameter is also called <code>bug</code>, will trigger a syntax
+              error in PhantomJS. Note that a function declaration has no
+              problems with it:
+            </p>
+
+            <pre>{`
+'use strict';
+function bug(bug) {}
+`}</pre>
+
+            <p>
+              No syntax error there! But when there <em>is</em> a syntax error,
+              PhantomJS will only say so and won't specify what the error is.
+              This can be kind of annoying when your automasted tests use
+              Phantom and fail miserably while the app works fine on your
+              browser. One good way to find the cause for these kind of bugs is
+              that PhantomJS behaves very similar to Safari, and this specific
+              bug is also present there. So if you happen to develop on Safari
+              then you'll see it before it gets to Phantom, but if you don't
+              it's a nice trick to remember the next time you have to debug a
+              Phantom-specific bug.
+            </p>
+          </div>
+        );
+      }}
+    </PostLayout>
+  );
+};
+
+export default Post;
