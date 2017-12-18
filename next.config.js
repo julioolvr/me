f08dd93956376bec51b513acb69e9f40ad15b7e4
Julio Olivera <julio.olvr@gmail.com>
2017-12-18 17:24:16 -0500

    Static HTML build

---
+++ next.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/me': { page: '/me' },
+      '/talks': { page: '/talks' },
+      '/things': { page: '/things' },
+      '/b': { page: '/b' },
+      '/b/2017/05/27/thoughts-about-vue-js': {
+        page: '/b/2017/05/27/thoughts-about-vue-js'
+      },
+      '/b/2016/10/29/request-persistence-express': {
+        page: '/b/2016/10/29/request-persistence-express'
+      },
+      '/b/2016/10/15/express-api-swagger': {
+        page: '/b/2016/10/15/express-api-swagger'
+      },
+      '/b/2016/10/01/express-jwt': { page: '/b/2016/10/01/express-jwt' },
+      '/b/2016/06/16/telegram-bot-fsm-async-await': {
+        page: '/b/2016/06/16/telegram-bot-fsm-async-await'
+      },
+      '/b/2015/10/15/angular-and-jst': {
+        page: '/b/2015/10/15/angular-and-jst'
+      },
+      '/b/2015/07/25/syntax-error-strict-mode-phantomjs': {
+        page: '/b/2015/07/25/syntax-error-strict-mode-phantomjs'
+      },
+      '/b/2014/09/03/fixed-sidebar-header-footer': {
+        page: '/b/2014/09/03/fixed-sidebar-header-footer'
+      },
+      '/b/2013/10/07/elasticsearch-recreate-index-tests': {
+        page: '/b/2013/10/07/elasticsearch-recreate-index-tests'
+      },
+      '/b/2013/02/17/lazy-js-method-evaluation': {
+        page: '/b/2013/02/17/lazy-js-method-evaluation'
+      }
+    };
+  }
+};
